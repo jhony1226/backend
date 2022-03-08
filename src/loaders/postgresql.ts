4f11a0598f1d183839e53e75b9f7d01fe5127ead
“jhony1226” <jhony1226@gmail.com>
2022-03-08 15:42:58 -0500

    m

--- src/loaders/postgresql.ts
+++ src/loaders/postgresql.ts
@@ -9,11 +9,11 @@ import { Pool } from 'pg';
 // });
 
 const db = new Pool({
-  host: 'localhost',
+  host: 'ec2-3-230-238-86.compute-1.amazonaws.com',
   port: 5432,
-  database: 'dbDelivery',
-  user: 'postgres',
-  password: '12345',
+  database: 'd9dmtfaqasu48s',
+  user: 'lipzvdhtieiiim',
+  password: '89321d78d7d0dfa64f7bc11498413f74bccb1b3c18b436a1dc66919ffe65bdc6',
 });
 
 export default db;
